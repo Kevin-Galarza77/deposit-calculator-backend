2506f30c98fb69b06457a8ea04d4c979ed0867de
Kevin-Galarza10 <jimenezkev1040@gmail.com>
2024-01-09 22:31:39 -0500

    calcular de detallaes de credito listo

--- app/Http/Controllers/CreditsDetail/ApiCreditDetailController.php
+++ app/Http/Controllers/CreditsDetail/ApiCreditDetailController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\CreditsDetail;
 
 use App\Http\Controllers\Controller;
+use App\Models\CreditDetail;
 use Illuminate\Http\Request;
 
 class ApiCreditDetailController extends Controller
@@ -17,6 +18,29 @@ class ApiCreditDetailController extends Controller
     {
         $creditDetail = new UpdateCreditDetailController();
         return $creditDetail->update($request, $id);
+    }
+
+    public function destroy($id)
+    {
+        $alert = 'No se pudo eliminar el detalle, intente nuevamente.';
+        $status = false;
+        $messages = [];
+        $data = [];
+
+        $credit_details = CreditDetail::find($id);
+        if (isset($credit_details)) {
+            if ($credit_details->delete()) {
+                $alert  =  'El detalle se ha eliminado correctamente!';
+                $status =  true;
+            }
+        }
+
+        return [
+            'alert'     =>  $alert,
+            'status'    =>  $status,
+            'messages'  =>  $messages,
+            'data'      =>  $data
+        ];
     }
     
 }
